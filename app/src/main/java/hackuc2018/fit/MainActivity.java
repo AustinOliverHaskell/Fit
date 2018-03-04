780bb7d01ec17518b204aadd9bed7f392b1f9a60
Austin Oliver Haskell <ahaskell@carthage.edu>
2018-03-04 03:45:00 -0500

    Database Object should be done

--- app/src/main/java/hackuc2018/fit/MainActivity.java
+++ app/src/main/java/hackuc2018/fit/MainActivity.java
@@ -1,53 +1,16 @@
 package hackuc2018.fit;
 
-import android.*;
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
 
-
-import java.net.URL;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -55,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private String username = "";
     private FirebaseAuth.AuthStateListener authStateListener;
 
-    DBManager db = new DBManager("test");
+    DBManager db = new DBManager("Austin");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
         {
             username = auth.getCurrentUser().getEmail();
         }
+
+        TextView tv = (TextView) findViewById(R.id.score);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.push("Austin");
+            }
+        });
+
     }
 
     private FirebaseAuth.AuthStateListener generateAuthListener()
